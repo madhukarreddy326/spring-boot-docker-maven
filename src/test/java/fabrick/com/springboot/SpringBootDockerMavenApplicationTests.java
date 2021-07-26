9d6d594017288a11ea42b84dfeca3ef2e76dbcef
Madhukar <madhukar.p@centrico.tech>
2021-07-26 09:41:12 +0530

    first commit

---
+++ src/test/java/fabrick/com/springboot/SpringBootDockerMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package fabrick.com.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
